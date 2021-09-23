135e677df78f3f9712cab2ebffd306f106f19a1a
Y0moo <its.dmitrii.kpl@gmail.com>
2021-09-23 17:59:01 +0200

    react-moralis syntax improves

--- src/components/CatList.js
+++ src/components/CatList.js
@@ -1,65 +1,63 @@
-import { Box, ImageList, ImageListItem } from '@mui/material'
-import { useMoralis } from 'react-moralis'
-import React, { useState, useEffect } from "react"
-import { makeStyles } from '@mui/styles'
+import { Box, ImageList, ImageListItem } from "@mui/material";
+import { useMoralis, useMoralisQuery } from "react-moralis";
+import React, { useState, useEffect } from "react";
+import { makeStyles } from "@mui/styles";
 
+const useStyles = makeStyles((theme) => ({
+  catImg: {
+    "&:hover": {
+      boxShadow: "10px 5px 5px black;",
+      backgroundColor: "blue",
+    },
+  },
+}));
 
-const useStyles = makeStyles(theme => ({
-    catImg: {
-        "&:hover": {
-            boxShadow: "10px 5px 5px black;",
-            backgroundColor: "blue",
-        }
-    }
-}))
-
-
-const baseOpenseaURL = "https://opensea.io/assets/"
-const baseTestnetOpenseaURL = "https://testnets.opensea.io/assets/"
-const baseURI = "https://us-central1-retro-cats.cloudfunctions.net/retro-cats-image-rinkeby?token_id="
+const baseOpenseaURL = "https://opensea.io/assets/";
+const baseTestnetOpenseaURL = "https://testnets.opensea.io/assets/";
+const baseURI =
+  "https://us-central1-retro-cats.cloudfunctions.net/retro-cats-image-rinkeby?token_id=";
 
 export const CatList = ({ catListData, retroCatsAddress, networkId }) => {
-    const classes = useStyles()
-    const { web3, user, Moralis } = useMoralis()
+  const classes = useStyles();
+  const { web3, user, Moralis } = useMoralis();
+  const { data: queryResults } = useMoralisQuery("NFTImage", (query) =>
+    query.equalTo("retroCatsAddress", retroCatsAddress)
+  );
 
-    const [imageURLs, setImageURLs] = useState({})
-    useEffect(() => { getImageUrls(retroCatsAddress) }, [])
+  const [imageURLs, setImageURLs] = useState({});
+  useEffect(() => {
+    if (queryResults) getImageUrls();
+  }, [queryResults]);
 
-    const getImageUrls = async (retroCatsAddress) => {
-        const NFTImage = Moralis.Object.extend("NFTImage")
-        const query = new Moralis.Query(NFTImage)
-        query.equalTo("retroCatsAddress", retroCatsAddress)
-        const results = await query.find()
-        let imageURLs = {}
-        results.forEach(element => {
-            let url = element.get("image").url()
-            let tokenId = element.get("tokenId")
-            imageURLs[tokenId] = url
-        })
-        setImageURLs(imageURLs)
-        return imageURLs
-    }
+  const getImageUrls = async () => {
+    let imageURLs = {};
+    queryResults.forEach((element) => {
+      let url = element.get("image").url();
+      let tokenId = element.get("tokenId");
+      imageURLs[tokenId] = url;
+    });
+    setImageURLs(imageURLs);
+    return imageURLs;
+  };
 
-    return (
-        <div>
-            {
-                catListData.result.length > 0 ?
-                    <ImageList cols={3} rowHeight={270}>
-                        {catListData.result.map(cat => (
-                            <ImageListItem className={classes.catImg} key={cat.token_id} sx={{ padding: 1 }}>
-                                {/* <a href={networkId === 1 ? baseOpenseaURL + retroCatsAddress + "/" + cat.token_id : baseTestnetOpenseaURL + retroCatsAddress + "/" + cat.token_id} target="_blank" rel="noreferrer"> */}
-                                <img
-                                    src={imageURLs[cat.token_id]}
-                                    alt="retrocat"
-                                    loading="lazy"
-                                    key={cat.token_id} />
-                                {/* </a> */}
-                            </ImageListItem>
-                            // 
-                        ))}
-                    </ImageList >
-                    : <Box>No cats found <br /> Maybe hit refresh?</Box>
-            }
-        </div>
-    )
-}
+  return (
+    <div>
+      {catListData.result.length > 0 ? (
+        <ImageList cols={3} rowHeight={270}>
+          {catListData.result.map((cat) => (
+            <ImageListItem className={classes.catImg} key={cat.token_id} sx={{ padding: 1 }}>
+              {/* <a href={networkId === 1 ? baseOpenseaURL + retroCatsAddress + "/" + cat.token_id : baseTestnetOpenseaURL + retroCatsAddress + "/" + cat.token_id} target="_blank" rel="noreferrer"> */}
+              <img src={imageURLs[cat.token_id]} alt="retrocat" loading="lazy" key={cat.token_id} />
+              {/* </a> */}
+            </ImageListItem>
+            //
+          ))}
+        </ImageList>
+      ) : (
+        <Box>
+          No cats found <br /> Maybe hit refresh?
+        </Box>
+      )}
+    </div>
+  );
+};
--- src/components/MyCats.js
+++ src/components/MyCats.js
@@ -1,62 +1,66 @@
 import {
-    useMoralis,
-    useMoralisWeb3Api,
-    useMoralisWeb3ApiCall
-} from 'react-moralis'
-import { Box, Button, Snackbar } from '@mui/material'
-import { makeStyles } from '@mui/styles'
+  useMoralis,
+  useMoralisCloudFunction,
+  useMoralisWeb3Api,
+  useMoralisWeb3ApiCall,
+} from "react-moralis";
+import { Box, Button, Snackbar } from "@mui/material";
+import { makeStyles } from "@mui/styles";
 
-import Alert from "@mui/lab/Alert"
-import helperConfig from "../helper-config.json"
-import React, { useState, useEffect } from "react"
-import { CatList } from "./CatList"
+import Alert from "@mui/lab/Alert";
+import helperConfig from "../helper-config.json";
+import React, { useState, useEffect } from "react";
+import { CatList } from "./CatList";
 
-const useStyles = makeStyles(theme => ({
-    default: {
-        padding: 1,
-    },
-}))
+const useStyles = makeStyles((theme) => ({
+  default: {
+    padding: 1,
+  },
+}));
+
+let shouldSleep = false;
 
 export const MyCats = ({ networkId, retroCatsAddress }) => {
-    const classes = useStyles()
-    const networkName = networkId ? helperConfig[String(networkId)] : "dev"
-    const Web3Api = useMoralisWeb3Api()
-    const { web3, user, Moralis } = useMoralis()
-    const [refreshButtonHit, setRefreshButtonHit] = useState(false)
-    const handleCloseSnack = () => {
-        setRefreshButtonHit(false)
-    }
+  const classes = useStyles();
+  const networkName = networkId ? helperConfig[String(networkId)] : "dev";
+  const Web3Api = useMoralisWeb3Api();
+  const { web3, user, Moralis } = useMoralis();
+  const [refreshButtonHit, setRefreshButtonHit] = useState(false);
+  const handleCloseSnack = () => {
+    setRefreshButtonHit(false);
+  };
 
-    const { fetch, data, error, isLoading } = useMoralisWeb3ApiCall(Web3Api.account.getNFTsForContract,
-        { chain: networkName, address: user.attributes.accounts, token_address: retroCatsAddress }
-    )
+  const { fetch, data, error, isLoading } = useMoralisWeb3ApiCall(
+    Web3Api.account.getNFTsForContract,
+    { chain: networkName, address: user.attributes.accounts, token_address: retroCatsAddress }
+  );
+  const { fetch: runCloudFunc } = useMoralisCloudFunction(
+    "updateNFTImages",
+    { networkId, retroCatsAddress, shouldSleep },
+    { autoFetch: false }
+  );
 
-    const updateNFTImages = () => {
-        setRefreshButtonHit(true)
-        let shouldSleep = false
-        Moralis.Cloud.run("updateNFTImages", { networkId, retroCatsAddress, shouldSleep })
-    }
+  const updateNFTImages = () => {
+    setRefreshButtonHit(true);
+    runCloudFunc();
+  };
 
-    return (
-        <Box textAlign='center'>
-            {error && <div>{error.message}</div>}
-            {data ? <CatList catListData={data} retroCatsAddress={retroCatsAddress} networkId={networkId} /> : <div>Loading...</div>}
-            <Button
-                onClick={async () => updateNFTImages()}
-                color="primary"
-                size="small">
-                Refresh
-            </Button>
-            <Snackbar
-                open={refreshButtonHit}
-                autoHideDuration={5000}
-                onClose={handleCloseSnack}
-            >
-                <Alert onClose={handleCloseSnack} severity="success">
-                    Refresh queued! Check back in a few minutes!
-                </Alert>
-            </Snackbar>
-        </Box>
-
-    )
-}
+  return (
+    <Box textAlign="center">
+      {error && <div>{error.message}</div>}
+      {data ? (
+        <CatList catListData={data} retroCatsAddress={retroCatsAddress} networkId={networkId} />
+      ) : (
+        <div>Loading...</div>
+      )}
+      <Button onClick={async () => updateNFTImages()} color="primary" size="small">
+        Refresh
+      </Button>
+      <Snackbar open={refreshButtonHit} autoHideDuration={5000} onClose={handleCloseSnack}>
+        <Alert onClose={handleCloseSnack} severity="success">
+          Refresh queued! Check back in a few minutes!
+        </Alert>
+      </Snackbar>
+    </Box>
+  );
+};
